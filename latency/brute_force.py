2e0bb61c7dd800d80b89b92d3dd56f35963781f2
shixun404 <shixun404@gmail.com>
2024-05-25 00:57:53 -0700

    update

--- latency/brute_force.py
+++ latency/brute_force.py
@@ -3,7 +3,11 @@ import random
 import itertools
 import os
 import pickle as pkl
+import numpy as np
 from tqdm import tqdm
+from scipy import stats
+import statistics
+
 def create_fully_connected_graph(n):
     """ Create a fully connected weighted graph with n nodes """
     G = nx.complete_graph(n)
@@ -13,7 +17,7 @@ def create_fully_connected_graph(n):
         G.edges[v,u]['weight'] = G.edges[u,v]['weight']  # Assign random positive weights
     return G
 
-def find_regular_subgraph(G, k,  graph_name=None, iterations=100):
+def find_regular_subgraph(G, k,  graph_name=None, iterations=1):
     """ Attempt to find a k-regular subgraph with the minimum diameter from a fully connected graph """
     n = G.number_of_nodes()
     best_diameter = float('inf')
@@ -28,41 +32,53 @@ def find_regular_subgraph(G, k,  graph_name=None, iterations=100):
 
 
     # for _ in pbar:
+    diameter_list = []
     for _ in range(iterations):
         # Randomly select edges to attempt to form a 4-regular graph
         possible_subgraph = nx.random_regular_graph(k, n)
         for u, v in possible_subgraph.edges():
             possible_subgraph.edges[u,v]['weight'] = G.edges[u,v]['weight']
             possible_subgraph.edges[v,u]['weight'] = G.edges[v,u]['weight']
-
         # pbar.set_description(f"id: {_:6d}, diameter: {diameter:4f}, best_diameter: {best_diameter:4f}")
         # Check if the graph is 4-regular and calculate diameter
-        if all(d == k for n, d in possible_subgraph.degree()):
-            try:
-                diameter = nx.diameter(possible_subgraph, weight='weight')
-                if diameter < best_diameter:
-                    best_diameter = diameter
-                    best_subgraph = possible_subgraph
-                    # print(best_diameter)
-                    # with open('best_'+graph_name, 'wb') as f:
-                    #     pkl.dump(best_subgraph, f)
-                    #     print('best')
-            except nx.NetworkXError:
-                # Graph is not connected, skip it
-                continue
-        
+        try:
+            diameter = nx.diameter(possible_subgraph, weight='weight')
+            diameter_list.append(diameter)
+            if diameter < best_diameter:
+                best_diameter = diameter
+                best_subgraph = possible_subgraph
+        except nx.NetworkXError:
+            # Graph is not connected, skip it
+            continue
+    # mean_value = np.mean(diameter_list)
+
+    # # Calculating the median
+    # median_value = np.median(diameter_list)
+
+    # # Calculating the mode using scipy.stats (returns the smallest mode in case of multi-modal data)
+    # mode_value = stats.mode(diameter_list)[0][0]  # stats.mode returns a ModeResult object
+
+    # # Alternatively, calculating the mode using the statistics module (also handles multi-modal lists well)
+    # mode_value_statistics = statistics.mode(diameter_list)  # This throws an error if there's no single mode
+
+    # # Output results
+    # print("Mean:", mean_value)
+    # print("Median:", median_value)
+    # print("Mode (scipy.stats):", mode_value)
+    # print("Mode (statistics):", mode_value_statistics)
     return best_subgraph, best_diameter
 
 # Main execution
-N = 20
-k = 4
-graph_name = f'G_N={N}.pkl'
-if graph_name not in os.listdir('.'):
-    G = create_fully_connected_graph(20)
-    with open(graph_name, 'wb') as f:
-        pkl.dump(G, f)
-else:
-    with open(graph_name, 'rb') as f:
-        G = pkl.load(f)
-best_subgraph, best_diameter = find_regular_subgraph(G, k, graph_name)
-print(f"Best diameter found: {best_diameter}")
+if __name__ == '__main__':
+    N = 20
+    k = 4
+    graph_name = f'G_N={N}.pkl'
+    if graph_name not in os.listdir('.'):
+        G = create_fully_connected_graph(20)
+        with open(graph_name, 'wb') as f:
+            pkl.dump(G, f)
+    else:
+        with open(graph_name, 'rb') as f:
+            G = pkl.load(f)
+    best_subgraph, best_diameter = find_regular_subgraph(G, k, graph_name)
+    print(f"Best diameter found: {best_diameter}")
