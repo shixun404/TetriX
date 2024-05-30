c15274980f72ac281f0f99d2f28c3255046299a1
shixun404 <shixun404@gmail.com>
2024-05-30 12:13:30 -0700

    update

--- latency/DQNAgent_graph.py
+++ latency/DQNAgent_graph.py
@@ -67,15 +67,15 @@ class DQNNetwork(nn.Module):
         start_id = state[:, -1].to(torch.int64)
         m = state[:, self.N * self.N * 2:-1].reshape(-1, self.N, 1)
 
-        theta_1_o = self.theta_1(m)
+        theta_1_o = self.theta_1(m) #  (Nxp)
         for i in range(self.T):
-            theta_2_o = self.theta_2(torch.bmm(adj, x))
+            theta_2_o = self.theta_2(torch.bmm(adj, x)) # (NxNxp) + (Nxpxp)
             reshaped_adj = adj.reshape(-1, 1)
             
-            theta_4_o = torch.relu(self.theta_4(reshaped_adj))
+            theta_4_o = torch.relu(self.theta_4(reshaped_adj)) #  (NxNxp)
             theta_4_o = theta_4_o.reshape(bs, self.N, self.N, self.p)
-            theta_3_i = torch.relu(theta_4_o).sum(dim=2)
-            theta_3_o = self.theta_3(theta_3_i)
+            theta_3_i = torch.relu(theta_4_o).sum(dim=2) # (NxNxp)
+            theta_3_o = self.theta_3(theta_3_i) #  (Nxpxp)
             # x = torch.relu(theta_1_o + theta_2_o + theta_3_o)
             x = torch.relu(theta_1_o + theta_2_o + theta_3_o)
 
@@ -90,12 +90,12 @@ class DQNNetwork(nn.Module):
             fc_theta_3_o = self.fc_theta_3(fc_theta_3_i)
             x_fc = torch.relu(fc_theta_1_o + fc_theta_2_o + fc_theta_3_o)
         
-        summed_x = torch.sum(x, dim=1, keepdim=True)
+        summed_x = torch.sum(x, dim=1, keepdim=True) # Nxp
         summed_x = summed_x.repeat(1, self.N, 1)
-        fc_summed_x = torch.sum(x_fc, dim=1, keepdim=True)
+        fc_summed_x = torch.sum(x_fc, dim=1, keepdim=True) # Nxp
         fc_summed_x = fc_summed_x.repeat(1, self.N, 1)
-        theta_9_o = self.theta_9(fc_summed_x)
-        theta_6_o = self.theta_6(summed_x)
+        theta_9_o = self.theta_9(fc_summed_x) # Nxpxp
+        theta_6_o = self.theta_6(summed_x) # Nxpxp
 
         # Gather selected elements based on indices
         fc_selected_elements = torch.gather(x_fc, 1, start_id.view(-1, 1, 1).expand(bs, 1, self.p))
@@ -110,19 +110,19 @@ class DQNNetwork(nn.Module):
         
         
         
-        fc_theta_7_o = self.fc_theta_7(fc_id_x)
-        fc_theta_8_o = self.fc_theta_8(x_fc)
+        fc_theta_7_o = self.fc_theta_7(fc_id_x) # Nxpxp
+        fc_theta_8_o = self.fc_theta_8(x_fc) # Nxpxp
 
-        theta_7_o = self.theta_7(id_x)
-        theta_8_o = self.theta_8(x)
+        theta_7_o = self.theta_7(id_x) # Nxpxp
+        theta_8_o = self.theta_8(x) # Nxpxp
         
         # Concatenate along the last dimension
         theta_5_in = torch.relu(torch.cat([fc_selected_adj, theta_6_o, fc_theta_7_o, fc_theta_8_o, theta_7_o, theta_8_o, theta_9_o], dim=2))
         
-        theta_10_in = torch.relu(self.theta_5(theta_5_in))
-        theta_11_in = torch.relu(self.theta_10(theta_10_in))
-        theta_11_out = torch.relu(self.theta_11(theta_11_in))
-        output = self.output(theta_11_out).reshape(-1, self.N)
+        theta_10_in = torch.relu(self.theta_5(theta_5_in)) # Nx(6p+1)x(6p+1)xp
+        theta_11_in = torch.relu(self.theta_10(theta_10_in)) # Nxpxpx32
+        theta_11_out = torch.relu(self.theta_11(theta_11_in)) # Nx32x32x32
+        output = self.output(theta_11_out).reshape(-1, self.N) # Nx32x32
         
         # output = self.theta_5(theta_5_in).reshape(-1, self.N)
         # print(output[0])
